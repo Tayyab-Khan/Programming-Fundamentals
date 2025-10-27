cf836e67840640a58e2161b4d81dc166890e772e
Tayyab-Khan <tayyabkhanbcit@gmail.com>
2025-10-27 14:40:05 +0500

    Solved Find Index of First Occurrence

---
+++ LeetCode/FindIndex.c
@@ -0,0 +1,29 @@
+#include <string.h>
+
+int strStr(char* haystack, char* needle) {
+    int hLen = strlen(haystack);
+    int nLen = strlen(needle);
+
+    if (nLen == 0) {
+        return 0;
+    }
+
+    if (nLen > hLen) {
+        return -1;
+    }
+
+    for (int i = 0; i <= hLen - nLen; i++) {
+        int j;
+        for (j = 0; j < nLen; j++) {
+            if (haystack[i + j] != needle[j]) {
+                break;
+            }
+        }
+        
+        if (j == nLen) {
+            return i;
+        }
+    }
+
+    return -1;
+}
